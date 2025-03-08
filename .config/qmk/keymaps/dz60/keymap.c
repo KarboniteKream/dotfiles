ce125e9c6ba38f6b97041d143e0f3e089a6c9f71
Klemen Košir <klemen.kosir@kream.io>
2025-03-08 19:44:56 +0900

    qmk: Update the dz60 keyboard

--- .config/qmk/keymaps/dz60/keymap.c
+++ .config/qmk/keymaps/dz60/keymap.c
@@ -7,23 +7,23 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layer
      * .-----------------------------------------------------------.
-     * |E`~|1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |Backsp |
+     * |E`~|1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |\  |`  |
      * |-----------------------------------------------------------|
-     * |Tab  |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  |]  |\    |
+     * |Tab  |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  |]  |Bcksp|
      * |-----------------------------------------------------------|
-     * |Fn    |A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  |Enter   |
+     * |Ctrl  |A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  |Enter   |
      * |-----------------------------------------------------------|
-     * |Shift   |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |Shift |   |
+     * |Shift   |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |Shift |Fn |
      * |-----------------------------------------------------------|
-     * |Ctrl|Alt |Win |Space     |Fn  |Space   |Caps|    |    |    |
+     * |    |Alt |Win |Space     |Fn  |Space   |Caps|    |    |    |
      * '-----------------------------------------------------------'
      */
     [_BL] = LAYOUT_60_ansi_split_space_rshift(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                    MO(_FL), KC_SPC,                    KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FL),
+        MO(_FL), KC_LALT, KC_LGUI, KC_SPC,                    MO(_FL), KC_SPC,                    KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     /* Arrow Layer
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '-----------------------------------------------------------'
      */
     [_AL] = LAYOUT_60_ansi_split_space_rshift(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,
@@ -49,22 +49,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Function Layer
      * .-----------------------------------------------------------.
-     * |`~ |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |
+     * |`~ |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|   |Del|
      * |-----------------------------------------------------------|
-     * |     |Prv|Ply|Nxt|   |   |Hom|PgD|PgU|End|PSc|`~ |~  |     |
+     * |     |Prv|Ply|Nxt|   |   |Hom|PgD|PgU|End|PSc|Up |   |     |
      * |-----------------------------------------------------------|
-     * |      |Vl-|Mut|Vl+|   |   |Lft|Dwn|Up |Rgt|   |   |        |
+     * |      |Vl-|Mut|Vl+|   |   |Lft|Dwn|Up |Rgt|Lft|Rgt|        |
      * |-----------------------------------------------------------|
-     * |        |Br-|Br+|   |   |   |   |   |   |   |   |      |Arr|
+     * |        |Br-|Br+|   |   |   |   |   |   |   |Dwn|      |Arr|
      * |-----------------------------------------------------------|
      * |    |    |    |          |    |        |    |    |    |    |
      * '-----------------------------------------------------------'
      */
     [_FL] = LAYOUT_60_ansi_split_space_rshift(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, KC_GRV,  KC_TILD, _______,
-        _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
-        _______,          KC_SLCK, KC_PAUS, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG(_AL),
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, KC_UP,   _______, _______,
+        _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEFT, KC_RGHT, _______,
+        _______,          KC_SCRL, KC_PAUS, _______, _______, _______, _______, _______, _______, _______, KC_DOWN, _______, TG(_AL),
         _______, _______, _______, _______,                   _______, _______,                   _______, _______, _______, _______
     ),
 };
