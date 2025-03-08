f2eac48e488ea8377668bbfee6b977ba4d68ba2e
Klemen Košir <klemen.kosir@kream.io>
2025-03-08 21:59:24 +0900

    qmk: Update the dz60 keyboard

--- .config/qmk/keyboards/dz60/keymaps/kream/config.h
+++ .config/qmk/keyboards/dz60/keymaps/kream/config.h
@@ -1,2 +1,6 @@
-/* Disable Grave Escape when Ctrl is held down */
+/* Use Escape as Bootmagic. */
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
+
+/* Disable Grave Escape when Ctrl is held down. */
 #define GRAVE_ESC_CTRL_OVERRIDE
--- .config/qmk/keyboards/dz60/keymaps/kream/keymap.c
+++ .config/qmk/keyboards/dz60/keymaps/kream/keymap.c
@@ -3,9 +3,36 @@
 #define _BL 0
 #define _AL 1
 #define _FL 2
+#define _RL 3
+
+enum my_keycodes {
+    MK_KANA = SAFE_RANGE,
+};
+
+void toggle_kana(keyrecord_t *record) {
+    if (!record->event.pressed) {
+        return;
+    }
+
+    if (detected_host_os() == OS_WINDOWS) {
+        SEND_STRING(SS_LALT("`"));
+    } else {
+        tap_code(KC_CAPS);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MK_KANA:
+            toggle_kana(record);
+            break;
+    }
+
+    return true;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base Layer
+    /* Base layer.
      * .-----------------------------------------------------------.
      * |E`~|1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |\  |`  |
      * |-----------------------------------------------------------|
@@ -15,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shift   |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |Shift |Fn |
      * |-----------------------------------------------------------|
-     * |    |Alt |Win |Space     |Fn  |Space   |Caps|    |    |    |
+     * |Ctrl|Alt |Meta|Space     |Fn  |Space   |Kana|    |    |    |
      * '-----------------------------------------------------------'
      */
     [_BL] = LAYOUT_60_ansi_split_space_rshift(
@@ -23,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FL),
-        MO(_FL), KC_LALT, KC_LGUI, KC_SPC,                    MO(_FL), KC_SPC,                    KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                    MO(_FL), KC_SPC,                    MK_KANA, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
-    /* Arrow Layer
+    /* Arrow layer.
      * .-----------------------------------------------------------.
      * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
      * |-----------------------------------------------------------|
@@ -47,24 +74,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______,                   _______, _______,                   _______, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    /* Function Layer
+    /* Function layer.
      * .-----------------------------------------------------------.
      * |`~ |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|   |Del|
      * |-----------------------------------------------------------|
-     * |     |Prv|Ply|Nxt|   |   |Hom|PgD|PgU|End|PSc|Up |   |     |
+     * |Kana |Prv|Ply|Nxt|   |   |Hom|PgD|PgU|End|PSc|Up |   |     |
      * |-----------------------------------------------------------|
-     * |      |Vl-|Mut|Vl+|   |   |Lft|Dwn|Up |Rgt|Lft|Rgt|        |
+     * |Caps  |Vl-|Mut|Vl+|   |   |Lft|Dwn|Up |Rgt|Lft|Rgt|        |
      * |-----------------------------------------------------------|
-     * |        |Br-|Br+|   |   |   |   |   |   |   |Dwn|      |Arr|
+     * |Root    |Br-|Br+|   |   |   |   |   |   |   |Dwn|      |   |
      * |-----------------------------------------------------------|
      * |    |    |    |          |    |        |    |    |    |    |
      * '-----------------------------------------------------------'
      */
     [_FL] = LAYOUT_60_ansi_split_space_rshift(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, KC_UP,   _______, _______,
-        _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEFT, KC_RGHT, _______,
-        _______,          KC_SCRL, KC_PAUS, _______, _______, _______, _______, _______, _______, _______, KC_DOWN, _______, TG(_AL),
+        MK_KANA, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, KC_UP,   _______, _______,
+        KC_CAPS, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEFT, KC_RGHT, _______,
+        MO(_RL),          KC_SCRL, KC_PAUS, _______, _______, _______, _______, _______, _______, _______, KC_DOWN, _______, _______,
+        _______, _______, _______, _______,                   _______, _______,                   _______, _______, _______, _______
+    ),
+
+    /* Root layer.
+     * .-----------------------------------------------------------.
+     * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
+     * |-----------------------------------------------------------|
+     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
+     * |-----------------------------------------------------------|
+     * |      |Arr|   |   |   |   |   |   |   |   |   |   |        |
+     * |-----------------------------------------------------------|
+     * |        |   |   |Cmd|   |Btl|   |   |   |   |   |      |   |
+     * |-----------------------------------------------------------|
+     * |    |    |    |          |    |        |    |    |    |    |
+     * '-----------------------------------------------------------'
+     */
+    [_RL] = LAYOUT_60_ansi_split_space_rshift(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, TG(_AL), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, AG_TOGG, _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______,                   _______, _______,                   _______, _______, _______, _______
     ),
 };
